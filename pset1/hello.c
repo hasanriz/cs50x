ee631f5542b2ed578cf37e03198ccc3b39e4d584
hasanriz <hasangr8@gmail.com>
2016-06-10 19:07:41 +0500

    Create hello.c

---
+++ pset1/hello.c
@@ -0,0 +1,20 @@
+/*
+ * hello.c
+ *  
+ * Author:hasanriz
+ *  
+ * This is my first ever program. WOOOHOOO!
+ * much success, very program, much WOW.
+ *  
+ * This is the solution for Hello,C in pset1.
+ *  
+ *  :)
+ */  
+
+#include <stdio.h>
+
+int main(void)
+{
+    //show hello, world
+    printf("hello, world\n");
+}
